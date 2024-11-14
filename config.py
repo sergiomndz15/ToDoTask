c4aab05234378324a653d5bab0da1f234c55de2d
sergiomndz15 <147667506+sergiomndz15@users.noreply.github.com>
2024-11-13 19:50:14 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb+srv://23300058:<db_password>@myapi.umx1u.mongodb.net/todo_app?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://23300058:qz220udeBOxjHaCG@myapi.umx1u.mongodb.net/todo_app?retryWrites=true&w=majority&tlsAllowInvalidCertificates=True")
     db = client['todo_app']
     return db
